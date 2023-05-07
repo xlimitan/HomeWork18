af5655550bcf7fd09f0c468004f98a461102f240
xlimitan <xlimitan@yandex.ru>
2023-05-07 13:25:38 +0300

    DZ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        for (Integer num : nums) {
+            if (num % 2 == 1) {
+                System.out.println(num);
+            }
+        }
+        List<Integer> nums1 = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        Set<Integer> set = new TreeSet<>();
+        for (Integer num1 : nums1) {
+            if (num1 % 2 == 0) {
+                set.add(num1);
+            }
+        }
+        System.out.println(set);
+
+        List<String> strings = new ArrayList<>(List.of("один", "один", "два","два", "два", "три", "три", "три"));
+        Set<String> set1 = new HashSet<>(strings);
+        System.out.println(set1);
+
+        List<String> strings1 = new ArrayList<>(List.of("один", "один", "два","два", "два", "три", "три", "три"));
+        Map<String, Integer> map = new HashMap<>();
+        for (String string : strings1) {
+            if (!map.containsKey(string)){
+                map.put(string, 1);
+            } else {
+                map.put(string, map.get(string) + 1);
+            }
+        }
+        System.out.println(map.values());
     }
 }
